404f1dc2d83b22d2680edb6f9288b531640a3540
fazil8848 <128672726+fazil8848@users.noreply.github.com>
2023-09-25 11:02:25 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // App.js
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/NavBar/Navbar";
 import Banner from "./Components/Banner/Banner";
 import Posters from "./Components/Posters/Posters";
-
+import { API_KEY } from "./constants/constants";
+import axios from "./axios";
 import {
   Originals,
   Action,
@@ -16,17 +17,34 @@ import {
 } from "./urls";
 
 function App() {
+  const [videoKey, setVideoKey] = useState('');
+  const [clickedComponent, setClickedComponent] = useState('');
+
+  const playHandler = (id, title) => {
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((res) => {
+        let key = res.data.results.length > 0 ? res.data.results[0].key : '';
+        setVideoKey(key);
+        setClickedComponent(title); // Set the clicked component title
+      })
+      .catch((error) => {
+        console.error(`Error fetching video for movie ID ${id}:`, error);
+        setVideoKey('');
+        setClickedComponent('');
+      });
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Banner />
-      <Posters title="Netflix Originals" url={Originals} />
-      <Posters title="Trending" size url={Trending} />
-      <Posters title="Action" size url={Action} />
-      <Posters title="Comedy" size url={Comedy} />
-      <Posters title="Romance" size url={Romance} />
-      <Posters title="Horror" size url={Horror} />
-      <Posters title="Documentaries" size url={Documentaries} />
+      <Posters title="Netflix Originals" url={Originals} playHandler={(id) => playHandler(id, "Netflix Originals")} videoKey={videoKey} clickedComponent={clickedComponent} />
+      <Posters title="Trending" size url={Trending} playHandler={(id) => playHandler(id, "Trending")} videoKey={videoKey} clickedComponent={clickedComponent} />
+      <Posters title="Action" size url={Action} playHandler={(id) => playHandler(id, "Action")} videoKey={videoKey} clickedComponent={clickedComponent} />
+      <Posters title="Comedy" size url={Comedy} playHandler={(id) => playHandler(id, "Comedy")} videoKey={videoKey} clickedComponent={clickedComponent} />
+      <Posters title="Romance" size url={Romance} playHandler={(id) => playHandler(id, "Romance")} videoKey={videoKey} clickedComponent={clickedComponent} />
+      <Posters title="Horror" size url={Horror} playHandler={(id) => playHandler(id, "Horror")} videoKey={videoKey} clickedComponent={clickedComponent} />
+      <Posters title="Documentaries" size url={Documentaries} playHandler={(id) => playHandler(id, "Documentaries")} videoKey={videoKey} clickedComponent={clickedComponent} />
     </div>
   );
 }
--- src/Components/Posters/Posters.jsx
+++ src/Components/Posters/Posters.jsx
@@ -2,11 +2,10 @@
 import React, { useEffect, useState } from "react";
 import "./Posters.css";
 import axios from "../../axios";
-import { API_KEY, imageUrl } from "../../constants/constants";
+import { imageUrl } from "../../constants/constants";
 import YouTube from "react-youtube";
 
 function Posters(props) {
-  const [videoKey, setVideoKey] = useState("");
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -16,24 +15,11 @@ function Posters(props) {
   }, [props.url]);
 
   const opts = {
-    height: "450",
-    width: "100%",
+    height: '450',
+    width: '100%',
     playerVars: {
       autoplay: 1,
     },
-  };
-
-  const playHandler = (id) => {
-    setVideoKey("");
-    axios
-      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
-      .then((res) => {
-        let key = res.data.results.length > 0 ? res.data.results[0].key : "";
-        setVideoKey(key);
-      })
-      .catch((error) => {
-        console.error(`Error fetching video for movie ID ${id}:`, error);
-      });
   };
 
   return (
@@ -47,12 +33,12 @@ function Posters(props) {
             alt="poster"
             key={obj.id}
             onClick={() => {
-              playHandler(obj.id);
+              props.playHandler(obj.id);
             }}
           />
         ))}
       </div>
-      {videoKey && <YouTube videoId={videoKey} opts={opts} />}
+      {props.clickedComponent === props.title && props.videoKey && <YouTube videoId={props.videoKey} opts={opts} />}
     </div>
   );
 }
